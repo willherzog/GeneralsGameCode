8df197a8e1a486596085d0291d40d0be4e4c65a6
OmniBlade <nadia_xy@hotmail.com>
2025-03-17 09:25:32 +0000

    Bumps version of miles sdk stub. (#440)
    Brings in fixes to API that solve audio issues.

--- cmake/miles.cmake
+++ cmake/miles.cmake
@@ -1,7 +1,7 @@
 FetchContent_Declare(
     miles
     GIT_REPOSITORY https://github.com/TheSuperHackers/miles-sdk-stub.git
-    GIT_TAG        564a0cb6dcf94bd17a1adf8f6110c108189efbae
+    GIT_TAG        0fef646a85c822475d55f19e3ca185263fb4a967
 )
 
 FetchContent_MakeAvailable(miles)
