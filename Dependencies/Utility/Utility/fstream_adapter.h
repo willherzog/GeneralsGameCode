6bc2760707f51f02f586e60ffd26754bf7c5b9ac
xezon <4720891+xezon@users.noreply.github.com>
2025-03-25 20:17:15 +0100

    [ZH] Fix fstream include errors for Zero Hour Tools build (#509)

---
+++ Dependencies/Utility/Utility/fstream_adapter.h
@@ -0,0 +1,14 @@
+// TheSuperHackers
+// This file includes the fstream that is compatible with vs6, STLPort and modern c++.
+
+#pragma once
+
+#ifdef USING_STLPORT
+
+#include <fstream.h>
+
+#else
+
+#include <fstream>
+
+#endif
