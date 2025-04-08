3feb3fcd6cdb4bbde767c4202b1fb7f4417abbb0
Mauller <26652186+Mauller@users.noreply.github.com>
2025-04-08 18:05:44 +0100

    [GEN][ZH] Fix uninitialized costSoFar variable in AIPathfind::findGoundPath() (#632)

--- GeneralsMD/Code/GameEngine/Source/GameLogic/AI/AIPathfind.cpp
+++ GeneralsMD/Code/GameEngine/Source/GameLogic/AI/AIPathfind.cpp
@@ -7192,7 +7192,7 @@ Path *Pathfinder::findGroundPath( const Coord3D *from,
 		const Int adjacent[5] = {0, 1, 2, 3, 0};
 		Bool neighborFlags[8] = {false, false, false, false, false, false, false};
 
-		UnsignedInt newCostSoFar;
+		UnsignedInt newCostSoFar = 0;
 
 		for( int i=0; i<numNeighbors; i++ )
 		{
