8edcde46fc7812e08b9f42f23b71ebf750e154f8
xezon <4720891+xezon@users.noreply.github.com>
2025-04-13 11:32:17 +0200

    [CMAKE] Split cmake options for build and memory into separate files (#666)

---
+++ cmake/config-build.cmake
@@ -0,0 +1,45 @@
+# Do we want to build extra SDK stuff or just the game binary?
+option(RTS_BUILD_ZEROHOUR "Build Zero Hour code." ON)
+option(RTS_BUILD_GENERALS "Build Generals code." ON)
+option(RTS_BUILD_OPTION_INTERNAL "Build code with the \"Internal\" configuration." OFF)
+option(RTS_BUILD_OPTION_PROFILE "Build code with the \"Profile\" configuration." OFF)
+option(RTS_BUILD_OPTION_DEBUG "Build code with the \"Debug\" configuration." OFF)
+option(RTS_BUILD_OPTION_ASAN "Build code with Address Sanitizer." OFF)
+
+if(NOT RTS_BUILD_ZEROHOUR AND NOT RTS_BUILD_GENERALS)
+    set(RTS_BUILD_ZEROHOUR TRUE)
+    message("You must select one project to build, building Zero Hour by default.")
+endif()
+
+add_feature_info(ZeroHourStuff RTS_BUILD_ZEROHOUR "Build Zero Hour code")
+add_feature_info(GeneralsStuff RTS_BUILD_GENERALS "Build Generals code")
+add_feature_info(InternalBuild RTS_BUILD_OPTION_INTERNAL "Building as an \"Internal\" build")
+add_feature_info(ProfileBuild RTS_BUILD_OPTION_PROFILE "Building as a \"Profile\" build")
+add_feature_info(DebugBuild RTS_BUILD_OPTION_DEBUG "Building as a \"Debug\" build")
+add_feature_info(AddressSanitizer RTS_BUILD_OPTION_ASAN "Building with address sanitizer")
+
+
+if(NOT IS_VS6_BUILD)
+    # Because we set CMAKE_CXX_STANDARD_REQUIRED and CMAKE_CXX_EXTENSIONS in the compilers.cmake this should be enforced.
+    target_compile_features(core_config INTERFACE cxx_std_20)
+endif()
+
+target_compile_options(core_config INTERFACE ${RTS_FLAGS})
+
+# This disables a lot of warnings steering developers to use windows only functions/function names.
+if(MSVC)
+    target_compile_definitions(core_config INTERFACE _CRT_NONSTDC_NO_WARNINGS _CRT_SECURE_NO_WARNINGS $<$<CONFIG:DEBUG>:_DEBUG_CRT>)
+endif()
+
+if(RTS_BUILD_OPTION_DEBUG)
+    target_compile_definitions(core_config INTERFACE _DEBUG WWDEBUG DEBUG)
+else()
+    target_compile_definitions(core_config INTERFACE _RELEASE)
+
+    if(RTS_BUILD_OPTION_INTERNAL)
+        target_compile_definitions(core_config INTERFACE _INTERNAL)
+    endif()
+    if(RTS_BUILD_OPTION_PROFILE)
+        target_compile_definitions(core_config INTERFACE _PROFILE)
+    endif()
+endif()
