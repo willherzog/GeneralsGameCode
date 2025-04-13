7977983830c6ac30f77e401a194e67f7754d2ea2
Mauller <26652186+Mauller@users.noreply.github.com>
2025-04-13 16:51:24 +0100

    [ZH] Fix weather settings memory leak in ~SnowManager() (#623)

--- GeneralsMD/Code/GameEngine/Source/GameClient/Snow.cpp
+++ GeneralsMD/Code/GameEngine/Source/GameClient/Snow.cpp
@@ -99,6 +99,13 @@ SnowManager::~SnowManager()
 {
 	delete [] m_startingHeights;
 	m_startingHeights=NULL;
+
+	// TheSuperHackers @fix Mauller 13/04/2025 Delete the instance of the weather settings
+	if (TheWeatherSetting)
+	{
+		((WeatherSetting*)TheWeatherSetting.getNonOverloadedPointer())->deleteInstance();
+		TheWeatherSetting=NULL;
+	}
 }
 
 OVERRIDE<WeatherSetting> TheWeatherSetting = NULL;
