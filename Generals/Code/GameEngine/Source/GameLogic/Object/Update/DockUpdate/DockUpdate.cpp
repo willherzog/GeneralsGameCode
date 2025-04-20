5005ed36022dd92b1b96d1694b49e9abf722b1b4
Mauller <26652186+Mauller@users.noreply.github.com>
2025-04-20 18:54:38 +0100

    [GEN][ZH] Fix uninitialized memory read in DockUpdate::loadDockPositions() (#731)
    Co-authored-by: Helmut Buhler <buhler@8gadgetpack.net>

--- Generals/Code/GameEngine/Source/GameLogic/Object/Update/DockUpdate/DockUpdate.cpp
+++ Generals/Code/GameEngine/Source/GameLogic/Object/Update/DockUpdate/DockUpdate.cpp
@@ -501,7 +501,9 @@ void DockUpdate::loadDockPositions()
 		if( m_numberApproachPositions != DYNAMIC_APPROACH_VECTOR_FLAG )
 		{
 			// Dynamic means no bones
-			Coord3D approachBones[DEFAULT_APPROACH_VECTOR_SIZE];
+			// TheSuperHackers @fix helmutbuhler 19/04/2025 Zero initialize array to prevent uninitialized memory reads.
+			// Important: the entire target vector is used for serialization and crc and must not contain random data.
+			Coord3D approachBones[DEFAULT_APPROACH_VECTOR_SIZE] = {0};
 			m_numberApproachPositionBones = myDrawable->getPristineBonePositions( "DockWaiting", 1, approachBones, NULL, m_numberApproachPositions);
 			if( m_numberApproachPositions == m_approachPositions.size() )//safeguard: will always be true
 			{
