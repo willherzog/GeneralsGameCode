a4fac78ce1fe75e060d48a26a1f38e8013f6b854
xezon <4720891+xezon@users.noreply.github.com>
2025-04-24 17:48:07 +0200

    [CMAKE] Move all RTS cmake options into early config-build.cmake to prevent cmake error on first generation (#753)

--- cmake/config-build.cmake
+++ cmake/config-build.cmake
@@ -1,4 +1,6 @@
 # Do we want to build extra SDK stuff or just the game binary?
+option(RTS_BUILD_CORE_TOOLS "Build core tools" ON)
+option(RTS_BUILD_CORE_EXTRAS "Build core extra tools/tests" OFF)
 option(RTS_BUILD_ZEROHOUR "Build Zero Hour code." ON)
 option(RTS_BUILD_GENERALS "Build Generals code." ON)
 option(RTS_BUILD_OPTION_INTERNAL "Build code with the \"Internal\" configuration." OFF)
@@ -11,6 +13,8 @@ if(NOT RTS_BUILD_ZEROHOUR AND NOT RTS_BUILD_GENERALS)
     message("You must select one project to build, building Zero Hour by default.")
 endif()
 
+add_feature_info(CoreTools RTS_BUILD_CORE_TOOLS "Build Core Mod Tools")
+add_feature_info(CoreExtras RTS_BUILD_CORE_EXTRAS "Build Core Extra Tools/Tests")
 add_feature_info(ZeroHourStuff RTS_BUILD_ZEROHOUR "Build Zero Hour code")
 add_feature_info(GeneralsStuff RTS_BUILD_GENERALS "Build Generals code")
 add_feature_info(InternalBuild RTS_BUILD_OPTION_INTERNAL "Building as an \"Internal\" build")
@@ -18,6 +22,25 @@ add_feature_info(ProfileBuild RTS_BUILD_OPTION_PROFILE "Building as a \"Profile\
 add_feature_info(DebugBuild RTS_BUILD_OPTION_DEBUG "Building as a \"Debug\" build")
 add_feature_info(AddressSanitizer RTS_BUILD_OPTION_ASAN "Building with address sanitizer")
 
+if(RTS_BUILD_ZEROHOUR)
+    option(RTS_BUILD_ZEROHOUR_TOOLS "Build tools for Zero Hour" ON)
+    option(RTS_BUILD_ZEROHOUR_EXTRAS "Build extra tools/tests for Zero Hour" OFF)
+    option(RTS_BUILD_ZEROHOUR_DOCS "Build documentation for Zero Hour" OFF)
+
+    add_feature_info(ZeroHourTools RTS_BUILD_ZEROHOUR_TOOLS "Build Zero Hour Mod Tools")
+    add_feature_info(ZeroHourExtras RTS_BUILD_ZEROHOUR_EXTRAS "Build Zero Hour Extra Tools/Tests")
+    add_feature_info(ZeroHourDocs RTS_BUILD_ZEROHOUR_DOCS "Build Zero Hour Documentation")
+endif()
+
+if(RTS_BUILD_GENERALS)
+    option(RTS_BUILD_GENERALS_TOOLS "Build tools for Generals" ON)
+    option(RTS_BUILD_GENERALS_EXTRAS "Build extra tools/tests for Generals" OFF)
+    option(RTS_BUILD_GENERALS_DOCS "Build documentation for Generals" OFF)
+
+    add_feature_info(GeneralsTools RTS_BUILD_GENERALS_TOOLS "Build Generals Mod Tools")
+    add_feature_info(GeneralsExtras RTS_BUILD_GENERALS_EXTRAS "Build Generals Extra Tools/Tests")
+    add_feature_info(GeneralsDocs RTS_BUILD_GENERALS_DOCS "Build Generals Documentation")
+endif()
 
 if(NOT IS_VS6_BUILD)
     # Because we set CMAKE_CXX_STANDARD_REQUIRED and CMAKE_CXX_EXTENSIONS in the compilers.cmake this should be enforced.
