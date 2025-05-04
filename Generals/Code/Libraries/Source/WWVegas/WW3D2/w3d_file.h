b3853c2b025fc91a291816987bc96f39a24966e4
xezon <4720891+xezon@users.noreply.github.com>
2025-05-04 20:28:29 +0200

    [GEN] Add FXShader, BINORMAL, TANGENT chunks from BFME (#813)

--- Generals/Code/Libraries/Source/WWVegas/WW3D2/w3d_file.h
+++ Generals/Code/Libraries/Source/WWVegas/WW3D2/w3d_file.h
@@ -309,6 +309,17 @@ June 5, 2001
   (gth) Adding line rendering options to particle systems today.  This involves a
   new line-properties chunk and a RenderMode variable added to the InfoV2 struct.
 
+TheSuperHackers @forwardport:
+April 5, 2025
+
+  Added W3D_CHUNK_FX_SHADERS and subchunks, which were added in BFME2. These 
+  chunks are used to define usage of a specific shader and pass params.
+
+April 21, 2025
+
+	Added W3D_CHUNK_VERTEX_TANGENT and W3D_CHUNK_VERTEX_BINORMAL chunks. These are used to define
+	normal mapping information for a mesh. They were added in BFME2.
+
 ********************************************************************************/
 
 
@@ -374,20 +385,28 @@ enum {
 				W3D_CHUNK_DCG								=0x0000003B,	// per-vertex diffuse color values (array of W3dRGBAStruct's)
 				W3D_CHUNK_DIG								=0x0000003C,	// per-vertex diffuse illumination values (array of W3dRGBStruct's)
 				W3D_CHUNK_SCG								=0x0000003E,	// per-vertex specular color values (array of W3dRGBStruct's)
+				W3D_CHUNK_FXSHADER_IDS						=0x0000003F,	// single or per-tri array of uint32 fx shader indices (check chunk size)
 
 				W3D_CHUNK_TEXTURE_STAGE					=0x00000048,	// wrapper around a texture stage.
 					W3D_CHUNK_TEXTURE_IDS				=0x00000049,	// single or per-tri array of uint32 texture indices (check chunk size)
 					W3D_CHUNK_STAGE_TEXCOORDS			=0x0000004A,	// per-vertex texture coordinates (array of W3dTexCoordStruct's)
 					W3D_CHUNK_PER_FACE_TEXCOORD_IDS	=0x0000004B,	// indices to W3D_CHUNK_STAGE_TEXCOORDS, (array of Vector3i)
 
-
+		W3D_CHUNK_FX_SHADERS 					=0x00000050,	// define an array of shaders to be used in the mesh
+			W3D_CHUNK_FX_SHADER					=0x00000051,	// a single shader entry
+				W3D_CHUNK_FX_SHADER_INFO		=0x00000052,	// information about the shader to be used (W3dFXShaderInfoStruct)
+				W3D_CHUNK_FX_SHADER_CONSTANT	=0x00000053,	// contains a constant name and value for the shader
+		
 		W3D_CHUNK_DEFORM									=0x00000058,	// mesh deform or 'damage' information.
 			W3D_CHUNK_DEFORM_SET							=0x00000059,	// set of deform information
 				W3D_CHUNK_DEFORM_KEYFRAME				=0x0000005A,	// a keyframe of deform information in the set
 					W3D_CHUNK_DEFORM_DATA				=0x0000005B,	// deform information about a single vertex
 
 		W3D_CHUNK_PS2_SHADERS							=0x00000080,	// Shader info specific to the Playstation 2.
-		
+
+		W3D_CHUNK_VERTEX_TANGENTS = 0x00000060,         // array of tangents (array of W3dVectorStruct's)
+		W3D_CHUNK_VERTEX_BINORMALS = 0x00000061,         // array of binormals (array of W3dVectorStruct's)
+				
 		W3D_CHUNK_AABTREE									=0x00000090,	// Axis-Aligned Box Tree for hierarchical polygon culling
 			W3D_CHUNK_AABTREE_HEADER,										// catalog of the contents of the AABTree
 			W3D_CHUNK_AABTREE_POLYINDICES,								// array of uint32 polygon indices with count=mesh.PolyCount
@@ -921,6 +940,24 @@ struct W3dPS2ShaderStruct
 	uint8						DParam;
 	uint8						pad[3];
 };
+
+struct W3dFXShaderInfoStruct
+{
+	char	ShaderName[W3D_NAME_LEN * 2];
+	uint8	Technique;
+	uint8	Pad[3];
+};
+
+typedef enum
+{
+	CONSTANT_TYPE_TEXTURE = 1,
+	CONSTANT_TYPE_FLOAT1 = 2,
+	CONSTANT_TYPE_FLOAT2 = 3,
+	CONSTANT_TYPE_FLOAT3 = 4,
+	CONSTANT_TYPE_FLOAT4 = 5,
+	CONSTANT_TYPE_INT = 6,
+	CONSTANT_TYPE_BOOL = 7
+} W3D_FX_SHADER_CONSTANT_TYPES;
 
 inline void W3d_Shader_Reset(W3dShaderStruct * s)									{	
 																										s->DepthCompare = W3DSHADER_DEPTHCOMPARE_PASS_LEQUAL;
