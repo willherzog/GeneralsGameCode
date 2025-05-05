afd9c30aaebda608bb630684b690ccc68a37a7f0
Mauller <26652186+Mauller@users.noreply.github.com>
2025-05-05 18:51:51 +0100

    [GEN][ZH] Fix possible division by zero in W3DCommandBarGenExpDraw() (#810)

--- GeneralsMD/Code/GameEngineDevice/Source/W3DDevice/GameClient/GUI/GUICallbacks/W3DControlBar.cpp
+++ GeneralsMD/Code/GameEngineDevice/Source/W3DDevice/GameClient/GUI/GUICallbacks/W3DControlBar.cpp
@@ -497,8 +497,14 @@ void W3DCommandBarGenExpDraw( GameWindow *window, WinInstanceData *instData )
 	static const Image *endBar = TheMappedImageCollection->findImageByName("GenExpBarTop1");
 	static const Image *beginBar = TheMappedImageCollection->findImageByName("GenExpBarBottom1");
 	static const Image *centerBar = TheMappedImageCollection->findImageByName("GenExpBar1");
-	Int progress;
-	progress = ((player->getSkillPoints() - player->getSkillPointsLevelDown()) * 100) /(player->getSkillPointsLevelUp() - player->getSkillPointsLevelDown());
+	Int progress = 0;
+	Int skillPointsRequired = player->getSkillPointsLevelUp() - player->getSkillPointsLevelDown();
+
+	// TheSuperHackers @bugfix Mauller 04/05/2025 Prevent possible division by zero
+	if ( skillPointsRequired > 0)
+	{
+		progress = ( ((player->getSkillPoints() - player->getSkillPointsLevelDown()) * 100) / skillPointsRequired );
+	}
 	
 	if(progress <= 0)
 		return;
