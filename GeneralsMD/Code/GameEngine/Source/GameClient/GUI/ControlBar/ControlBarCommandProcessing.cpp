9c20c27be58463a755b8c67a50f1fdf146a33b68
Mauller <26652186+Mauller@users.noreply.github.com>
2025-05-25 12:24:11 +0100

    [GEN][ZH] Fix global-buffer-overflow at GUI_COMMAND_EXIT_CONTAINER in ControlBar::processCommandUI() (#922)
    Co-authored-by: Caball009 <Caball009@users.noreply.github.com>

--- GeneralsMD/Code/GameEngine/Source/GameClient/GUI/ControlBar/ControlBarCommandProcessing.cpp
+++ GeneralsMD/Code/GameEngine/Source/GameClient/GUI/ControlBar/ControlBarCommandProcessing.cpp
@@ -681,20 +681,29 @@ CBCommandStatus ControlBar::processCommandUI( GameWindow *control,
 		case GUI_COMMAND_EXIT_CONTAINER:
 		{
 			Int i;
-			ObjectID objID;
+			// TheSuperHackers @fix Caball009/Mauller 23/05/2025 Fix uninitialized variable and control lookup behaviour to prevent a buffer-overflow when the control container is empty
+			ObjectID objID = INVALID_ID;
 
 			//
 			// find the object ID that wants to exit by scanning through the transport data and looking
 			// for the matching control button
 			//
-			for( i = 0; i < MAX_COMMANDS_PER_SET; i++ )
-				if( m_containData[ i ].control == control )
+			for (i = 0; i < MAX_COMMANDS_PER_SET; i++)
+			{
+				if (m_containData[i].control == control)
+				{
 					objID = m_containData[ i ].objectID;
+					break;
+				}
+			}
+
+			if (objID == INVALID_ID)
+				break;
 
 			// get the actual object
 			Object *objWantingExit = TheGameLogic->findObjectByID( objID );
 			
-			// if object is not found remove inventory entry and exit
+			// if the control container returns an object ID but the object is not found, remove the control entry and exit
 			if( objWantingExit == NULL )
 			{
 
