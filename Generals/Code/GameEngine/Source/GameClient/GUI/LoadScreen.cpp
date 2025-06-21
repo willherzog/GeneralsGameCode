af35ed98b026ee8cda13da8d17fef2541d89145a
xezon <4720891+xezon@users.noreply.github.com>
2025-06-21 11:11:24 +0200

    [GEN][ZH] Prevent reading invalid data in LoadScreen (#1090)

--- Generals/Code/GameEngine/Source/GameClient/GUI/LoadScreen.cpp
+++ Generals/Code/GameEngine/Source/GameClient/GUI/LoadScreen.cpp
@@ -886,6 +886,7 @@ void MultiPlayerLoadScreen::processProgress(Int playerId, Int percentage)
 	if( percentage < 0 || percentage > 100 || playerId >= MAX_SLOTS || playerId < 0 || m_playerLookup[playerId] == -1)
 	{
 		DEBUG_ASSERTCRASH(FALSE, ("Percentage %d was passed in for Player %d\n", percentage, playerId));
+		return;
 	}
 	//DEBUG_LOG(("Percentage %d was passed in for Player %d (in loadscreen position %d)\n", percentage, playerId, m_playerLookup[playerId]));
 	if(m_progressBars[m_playerLookup[playerId]])
@@ -1213,6 +1214,7 @@ void GameSpyLoadScreen::processProgress(Int playerId, Int percentage)
 	if( percentage < 0 || percentage > 100 || playerId >= MAX_SLOTS || playerId < 0 || m_playerLookup[playerId] == -1)
 	{
 		DEBUG_ASSERTCRASH(FALSE, ("Percentage %d was passed in for Player %d\n", percentage, playerId));
+		return;
 	}
 	//DEBUG_LOG(("Percentage %d was passed in for Player %d (in loadscreen position %d)\n", percentage, playerId, m_playerLookup[playerId]));
 	if(m_progressBars[m_playerLookup[playerId]])
@@ -1365,6 +1367,7 @@ void MapTransferLoadScreen::processProgress(Int playerId, Int percentage, AsciiS
 	if( percentage < 0 || percentage > 100 || playerId >= MAX_SLOTS || playerId < 0 || m_playerLookup[playerId] == -1)
 	{
 		DEBUG_ASSERTCRASH(FALSE, ("Percentage %d was passed in for Player %d\n", percentage, playerId));
+		return;
 	}
 
 	if (m_oldProgress[playerId] == percentage)
