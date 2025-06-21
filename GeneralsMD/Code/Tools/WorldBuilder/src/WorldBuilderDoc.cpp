2a179d8add10ce05c386c5358aeb7add631f73a4
Caball009 <82909616+Caball009@users.noreply.github.com>
2025-06-21 11:02:39 +0200

    [ZH] Fix heap-use-after-free in LayersList::updateUIFromList() when creating a new map in World Builder (#1098)

--- GeneralsMD/Code/Tools/WorldBuilder/src/WorldBuilderDoc.cpp
+++ GeneralsMD/Code/Tools/WorldBuilder/src/WorldBuilderDoc.cpp
@@ -1271,13 +1271,15 @@ BOOL CWorldBuilderDoc::OnNewDocument()
 	m_waypointTableNeedsUpdate = true;
 	m_curWaypointID = 0;
 	WbApp()->selectPointerTool();
-	PolygonTrigger::deleteTriggers();
 
 	// Make sure that all the old units are removed from the list.
 	// Bug fix by MLL 1/14/03
 	TheLayersList->enableUpdates();
 	TheLayersList->resetLayers();
 	TheLayersList->disableUpdates();
+
+	// TheSuperHackers @bugfix Caball009 20/06/2025 Must not delete polygon triggers before calling enableUpdates.
+	PolygonTrigger::deleteTriggers();
 
 	TheSidesList->clear();
 	TheSidesList->validateSides();
