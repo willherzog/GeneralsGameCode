da3346591cfd0c6761a9ecffc2cc3ad23bbb9122
xezon <4720891+xezon@users.noreply.github.com>
2025-06-22 08:44:33 +0200

    [GEN][ZH] Prevent using uninitialized memory 'delta' in Dict::ensureUnique() (#1131)

--- Generals/Code/GameEngine/Source/Common/Dict.cpp
+++ Generals/Code/GameEngine/Source/Common/Dict.cpp
@@ -182,7 +182,7 @@ Dict::DictPair *Dict::ensureUnique(int numPairsNeeded, Bool preserveData, DictPa
 		}
 	}
 
-	Int delta;
+	Int delta = 0;
 	if (pairToTranslate && m_data)
 		delta = pairToTranslate - m_data->peek();
 
