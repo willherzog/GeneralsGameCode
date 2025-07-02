b3f86e4117474efa82d3811f660cf7e39d820e63
xezon <4720891+xezon@users.noreply.github.com>
2025-07-02 22:39:32 +0200

    [CMAKE] Fix PDB size bloat in unoptimized builds (#1183)

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -20,6 +20,9 @@ if(MSVC)
     # Create PDB for Release as long as debug info was generated during compile.
     string(APPEND CMAKE_EXE_LINKER_FLAGS_RELEASE " /DEBUG /OPT:REF /OPT:ICF")
     string(APPEND CMAKE_SHARED_LINKER_FLAGS_RELEASE " /DEBUG /OPT:REF /OPT:ICF")
+    
+    # /INCREMENTAL:NO prevents PDB size bloat in Debug configuration(s).
+    add_link_options("/INCREMENTAL:NO")
 else()
     # We go a bit wild here and assume any other compiler we are going to use supports -g for debug info.
     string(APPEND CMAKE_CXX_FLAGS_RELEASE " -g")
