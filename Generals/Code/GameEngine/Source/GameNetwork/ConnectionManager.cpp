f4a05b5acb1a78b1550895c40054afb58372e08e
Bart Roossien | SkyAero <roossienb@users.noreply.github.com>
2025-07-20 23:03:54 +0200

    [GEN][ZH] Fix broken Player muting for Observer (#1307)

--- Generals/Code/GameEngine/Source/GameNetwork/ConnectionManager.cpp
+++ Generals/Code/GameEngine/Source/GameNetwork/ConnectionManager.cpp
@@ -661,7 +661,7 @@ void ConnectionManager::processChat(NetChatCommandMsg *msg)
 	
 	Bool fromObserver = !player->isPlayerActive();
 	Bool amIObserver = !ThePlayerList->getLocalPlayer()->isPlayerActive();
-	Bool canSeeChat = amIObserver || !fromObserver && !TheGameInfo->getConstSlot(playerID)->isMuted();
+	Bool canSeeChat = (amIObserver || !fromObserver) && !TheGameInfo->getConstSlot(playerID)->isMuted();
 	
 	if ( ((1<<m_localSlot) & msg->getPlayerMask() ) && canSeeChat  )
 	{
