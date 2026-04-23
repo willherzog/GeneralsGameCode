6375df6178116765c39c842ab3ecd1c6e698dafc
Will Herzog <willherzog@gmail.com>
2026-04-23 13:37:56 -0600

    Update CampaignManager.cpp

--- GeneralsMD/Code/GameEngine/Source/GameClient/System/CampaignManager.cpp
+++ GeneralsMD/Code/GameEngine/Source/GameClient/System/CampaignManager.cpp
@@ -71,7 +71,7 @@ CampaignManager *TheCampaignManager = nullptr;
 const FieldParse CampaignManager::m_campaignFieldParseTable[] =
 {
 
-	{ "Mission",						CampaignManager::parseMissionPart,	nullptr, nullptr },
+	{ "Mission",						CampaignManager::parseMissionPart,	nullptr, 0 },
 	{ "FirstMission",				INI::parseAsciiString,							nullptr,	offsetof( Campaign, m_firstMission )  },
 	{ "CampaignNameLabel",	INI::parseAsciiString,							nullptr, offsetof( Campaign, m_campaignNameLabel ) },
 	{ "FinalVictoryMovie",	INI::parseAsciiString,							nullptr, offsetof( Campaign, m_finalMovieName ) },
